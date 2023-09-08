52e5870018d8deff11eca82793feb6f884f3768d
reginald-todd <142221302+reginald-todd@users.noreply.github.com>
2023-09-08 19:33:15 +0200

    Add files via upload

--- calculations.sql
+++ calculations.sql
@@ -1,21 +1,16 @@
-
 --AB 1
 
---select * from THESISi.dbo.AB
+--SELECT * FROM THESISi.dbo.AB
 
-
-
-SELECT COUNT(TotalYears) as 'AB 1 Total Years > 5'
+SELECT COUNT(TotalYears) AS 'AB 1 Total Years > 5'
 FROM THESISi.dbo.ab
 WHERE TotalYears >= 5;
 
+SELECT COUNT(Reason) AS 'AB 1 no reason given' FROM THESISi.dbo.AB WHERE Reason = 'No reason given.'
 
-select count(Reason) as 'AB 1 no reason given' from THESISi.dbo.AB where Reason = 'No reason given.'
+SELECT COUNT(Type) AS 'AB 1 Total Boil Water Advisories' FROM THESISi.dbo.AB WHERE Type = 'Boil Water' 
 
-select count(Type) as 'AB 1 Total Boil Water Advisories' from THESISi.dbo.AB where Type = 'Boil Water' 
-
-select count(Type) as 'AB 1 Total Do Not Consume' from THESISi.dbo.AB where Type = 'Do Not Consume';
-
+SELECT COUNT(Type) AS 'AB 1 Total Do Not Consume' FROM THESISi.dbo.AB WHERE Type = 'Do Not Consume';
 
 
 WITH Period1 AS
@@ -75,8 +70,6 @@ FROM Period1 P1
 JOIN Period3 P3 ON 1 = 1;
 
 
-
-
 WITH Period1 AS
 (
     SELECT
@@ -96,66 +89,57 @@ SELECT
 FROM Period1 P1
 CROSS JOIN Period3 P3;
 
-
-
 --BC 1
 
 --select * from THESISi.dbo.BC
 
+SELECT COUNT(TotalYears) AS 'BC 1 Total Years > 5' 
+FROM THESISi.dbo.BC 
+WHERE TotalYears >= 5 
 
-
-select count(TotalYears) as 'BC 1 Total Years > 5' 
-from THESISi.dbo.BC 
-where TotalYears >= 5 
-
-
-
-Select Distinct(TotalYears), Count(TotalYears)
-From THESISi.dbo.BC
-Group by TotalYears
-
-
+SELECT DISTINCT(TotalYears), COUNT(TotalYears)
+FROM THESISi.dbo.BC
+GROUP BY TotalYears
 
 --FHA 1
 
---select * from THESISi.dbo.FHA
+--SELECT * FROM THESISi.dbo.FHA
 
-SELECT COUNT(TotalYears) as 'FHA 1 Total Years > 5'
+SELECT COUNT(TotalYears) AS 'FHA 1 Total Years > 5'
 FROM THESISi.dbo.FHA
 WHERE TotalYears >= 5;
 
 
-
 --FNHA
 
---select * from THESISi.dbo.FNHA
+--SELECT * FROM THESISi.dbo.FNHA
 
-select count(AdvisoryType) as 'FNHA 1 Total Water Quality Advisories' from THESISi.dbo.FNHA where AdvisoryType = 'Water Quality Advisory' 
+SELECT COUNT(AdvisoryType) AS 'FNHA 1 Total Water Quality Advisories' FROM THESISi.dbo.FNHA WHERE AdvisoryType = 'Water Quality Advisory' 
 
-select count(AdvisoryType) as 'FNHA 1 Total Boil Water Advisories' from THESISi.dbo.FNHA where AdvisoryType = 'Boil Water Advisory' 
+SELECT COUNT(AdvisoryType) AS 'FNHA 1 Total Boil Water Advisories' FROM THESISi.dbo.FNHA WHERE AdvisoryType = 'Boil Water Advisory' 
 
-select count(AdvisoryType) as 'FNHA 1 Total Do Not Consume' from THESISi.dbo.FNHA where AdvisoryType = 'Do Not Consume' 
+SELECT COUNT(AdvisoryType) AS 'FNHA 1 Total Do Not Consume' FROM THESISi.dbo.FNHA WHERE AdvisoryType = 'Do Not Consume' 
 
 
 
---IHA i
+--IHA 1
 
-select * from THESISi.dbo.IHA
-select * from THESISiii.dbo.IHA
+SELECT * FROM THESISi.dbo.IHA
+SELECT * FROM THESISiii.dbo.IHA
 
 --FIX:
-select count([Reason(s) for Advisory]) as 'IHA 1 Reason for advisory = NULL' from THESISi.dbo.IHA where [Reason(s) for Advisory] = 'NULL'
+SELECT COUNT([Reason(s) for Advisory]) AS 'IHA 1 Reason for advisory = NULL' FROM THESISi.dbo.IHA WHERE [Reason(s) for Advisory] = 'NULL'
 
 SELECT DISTINCT [Reason(s) for Advisory]
 FROM THESISi.dbo.IHA;
 
-select count([Reason(s) for Advisory]) as 'IHA 1 Reason for advisory = Untreated drinking water at risk' from THESISi.dbo.IHA where [Reason(s) for Advisory] = 'Untreated drinking water at risk of containing pathogens'
+SELECT COUNT([Reason(s) for Advisory]) AS 'IHA 1 Reason for advisory = Untreated drinking water at risk' FROM THESISi.dbo.IHA WHERE [Reason(s) for Advisory] = 'Untreated drinking water at risk of containing pathogens'
 
-select count([Current Advisory]) as 'IHA 1 Total Water Quality Advisories' from THESISi.dbo.IHA where [Current Advisory] = 'Water Quality Advisory' 
+SELECT COUNT([Current Advisory]) AS 'IHA 1 Total Water Quality Advisories' FROM THESISi.dbo.IHA WHERE [Current Advisory] = 'Water Quality Advisory' 
 
-select count([Current Advisory]) as 'IHA 1 Total Boil Water Advisories' from THESISi.dbo.IHA where [Current Advisory] = 'Boil Water Advisory' 
+SELECT COUNT([Current Advisory]) AS 'IHA 1 Total Boil Water Advisories' FROM THESISi.dbo.IHA WHERE [Current Advisory] = 'Boil Water Advisory' 
 
-select count([Current Advisory]) as 'IHA 1 Total Do Not Consume' from THESISi.dbo.IHA where [Current Advisory] = 'Do Not Consume';
+SELECT COUNT([Current Advisory]) AS 'IHA 1 Total Do Not Consume' FROM THESISi.dbo.IHA WHERE [Current Advisory] = 'Do Not Consume';
 
 
 
@@ -186,15 +170,13 @@ SELECT
     P3.[IHA 3 Total Years > 5 percentage] - P1.[IHA 1 Total Years > 5 percentage] AS 'Difference in Percentage'
 FROM Period1 P1
 JOIN Period3 P3 ON 1 = 1;
-
+GO
 
 
 
 SELECT
-        COUNT(*) AS 'IHA 2 Total Years'
+    COUNT(*) AS 'IHA 2 Total Years'
     FROM THESISii.dbo.IHA IHA2;
-
-
 
 
 WITH Period1 AS
@@ -215,29 +197,29 @@ SELECT
     P1.[IHA 1 Total Years] - P3.[IHA 3 Total Years] AS 'Difference in Total Years'
 FROM Period1 P1
 CROSS JOIN Period3 P3;
-
+GO
 
 
 
 --MB 1
 
-select * from THESISi.dbo.MB
+SELECT * FROM THESISi.dbo.MB
 
-select count(Type) as 'MB 1 Total Water Quality Advisories' from THESISi.dbo.MB where Type = 'Water Quality Advisory' 
+SELECT COUNT(Type) AS 'MB 1 Total Water Quality Advisories' FROM THESISi.dbo.MB WHERE Type = 'Water Quality Advisory' 
 
-select count(Type) as 'IHA 1 Total Boil Water Advisories' from THESISi.dbo.MB where Type = 'Boil Water' 
+SELECT COUNT(Type) AS 'IHA 1 Total Boil Water Advisories' FROM THESISi.dbo.MB WHERE Type = 'Boil Water' 
 
-select count(Type) as 'IHA 1 Total Do Not Consume' from THESISi.dbo.MB where Type = 'Do Not Consume' 
+SELECT COUNT(Type) AS 'IHA 1 Total Do Not Consume' FROM THESISi.dbo.MB WHERE Type = 'Do Not Consume' 
 
-SELECT COUNT(TotalYears) as 'MB 1 Total Years Greater Than 5'
+SELECT COUNT(TotalYears) AS 'MB 1 Total Years Greater Than 5'
 FROM THESISi.dbo.MB
 WHERE TotalYears >= 5;
 
-SELECT COUNT(TotalYears) as 'MB 1 Total Years > 10'
+SELECT COUNT(TotalYears) AS 'MB 1 Total Years > 10'
 FROM THESISi.dbo.MB
 WHERE TotalYears >= 10;
 
-SELECT COUNT(TotalYears) as 'MB 1 Total Years'
+SELECT COUNT(TotalYears) AS 'MB 1 Total Years'
 FROM THESISi.dbo.MB
 
 
@@ -252,28 +234,26 @@ FROM THESISi.dbo.MB;
 
 --MB 2
 
-select * from THESISii.dbo.MB
+SELECT * FROM THESISii.dbo.MB
 
 
 
 
 --MB 3
 
-select * from THESISiii.dbo.MB
+SELECT * FROM THESISiii.dbo.MB
 
+SELECT COUNT(Type) AS 'MB 1 Total Water Quality Advisories' FROM THESISiii.dbo.MB WHERE Type = 'Water Quality Advisory' 
 
+SELECT COUNT(Type) AS 'MB 1 Total Boil Water Advisories' FROM THESISiii.dbo.MB WHERE Type = 'Boil Water' 
 
-select count(Type) as 'MB 1 Total Water Quality Advisories' from THESISiii.dbo.MB where Type = 'Water Quality Advisory' 
+SELECT COUNT(Type) AS 'MB 1 Total Do Not Consume' FROM THESISiii.dbo.MB WHERE Type = 'Do Not Consume' 
 
-select count(Type) as 'MB 1 Total Boil Water Advisories' from THESISiii.dbo.MB where Type = 'Boil Water' 
-
-select count(Type) as 'MB 1 Total Do Not Consume' from THESISiii.dbo.MB where Type = 'Do Not Consume' 
-
-SELECT COUNT(TotalYears) as 'MB 1 Total Years Greater Than 5'
+SELECT COUNT(TotalYears) AS 'MB 1 Total Years Greater Than 5'
 FROM THESISiii.dbo.MB
 WHERE TotalYears >= 5;
 
-SELECT COUNT(TotalYears) as 'MB 1 Total Years > 10'
+SELECT COUNT(TotalYears) AS 'MB 1 Total Years > 10'
 FROM THESISiii.dbo.MB
 WHERE TotalYears >= 10;
 
@@ -286,8 +266,6 @@ SELECT
     SUM(CASE WHEN TotalYears >= 5 THEN 1 ELSE 0 END) AS 'MB 1 Total Years > 5',
     (CAST(SUM(CASE WHEN TotalYears >= 5 THEN 1 ELSE 0 END) AS FLOAT) / COUNT(*)) * 100 AS 'MB 1 Total Years > 5 percentage'
 FROM THESISiii.dbo.MB;
-
-
 
 
 WITH Period1 AS
@@ -316,7 +294,7 @@ SELECT
     P3.[MB 3 Total Years > 5 percentage] - P1.[MB 1 Total Years > 5 percentage] AS 'Difference in Percentage'
 FROM Period1 P1
 JOIN Period3 P3 ON 1 = 1;
-
+GO
 
 
 
@@ -345,4 +323,5 @@ SELECT
     P3.[MB 3 Total Years > 10 percentage] AS 'MB 3 Total Years > 10 percentage',
     P3.[MB 3 Total Years > 10 percentage] - P1.[MB 1 Total Years > 10 percentage] AS 'Difference in Percentage'
 FROM Period1 P1
-JOIN Period3 P3 ON 1 = 1;  -- Join on a constant value to get cross join effect
+JOIN Period3 P3 ON 1 = 1;
+GO
