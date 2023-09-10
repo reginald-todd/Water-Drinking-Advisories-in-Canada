23f2391a069cdefd634cd9296e50b517a6157c39
reginald-todd <142221302+reginald-todd@users.noreply.github.com>
2023-09-10 20:58:52 +0200

    Add files via upload

--- data cleaning.sql
+++ data cleaning.sql
@@ -1,363 +1,292 @@
 
---2022.01.07
+2022.01.07
 
---AB i
+AB i
 
-select * from THESISi.dbo.AB
+SELECT * FROM THESISi.dbo.AB
 
 ALTER TABLE THESISi.dbo.AB
-add SinceConverted Date;
+ADD SinceConverted DATE;
 
---Update THESISi.dbo.AB
---SET SinceConverted = CONVERT(Date,Since)
+UPADTE THESISi.dbo.AB
+SET SinceConverted = CONVERT(DATE,Since)
 
---ALTER TABLE THESISi.dbo.AB
---DROP column Since
-
-
-
+ALTER TABLE THESISi.dbo.AB
+DROP COLUMN Since
 
 
 ALTER TABLE THESISi.dbo.AB
-add TotalDays int
+ADD TotalDays INT
 
---update THESISi.dbo.AB
---SET TotalDays = DATEDIFF(day,since,'2022/01/07')
+UPDATE THESISi.dbo.AB
+SET TotalDays = DATEDIFF(DAY,since,'2022/01/07')
 
 
 
 ALTER TABLE THESISi.dbo.AB
-add TotalYears int
+ADD TotalYears INT
 
---update THESISi.dbo.AB
---SET TotalYears = DATEDIFF(year,since,'2022/01/07')
-
-
+UPDATE THESISi.dbo.AB
+SET TotalYears = DATEDIFF(YEAR,since,'2022/01/07')
 
 
+BC i
 
-
-
-
-
-
---BC i
-
-select * from THESISi.dbo.BC
+SELECT * FROM THESISi.dbo.BC
 
 
 ALTER TABLE THESISi.dbo.BC
-add SinceConverted Date;
+ADD SinceConverted DATE;
 
---Update THESISi.dbo.BC
---SET SinceConverted = CONVERT(Date,Since)
-
---ALTER TABLE THESISi.dbo.BC
---DROP column Since
-
-
+UPDATE THESISi.dbo.BC
+SET SinceConverted = CONVERT(DATE,Since)
 
 ALTER TABLE THESISi.dbo.BC
-add TotalDays int
-
---update THESISi.dbo.BC
---SET TotalDays = DATEDIFF(day,since,'2022/01/07')
-
-
+DROP COLUMN Since
 
 ALTER TABLE THESISi.dbo.BC
-add TotalYears int
+ADD TotalDays INT
 
---update THESISi.dbo.BC
---SET TotalYears = DATEDIFF(year,since,'2022/01/07')
+UPDATE THESISi.dbo.BC
+SET TotalDays = DATEDIFF(DAY,since,'2022/01/07')
 
+ALTER TABLE THESISi.dbo.BC
+ADD TotalYears INT
 
-
-
-
-
---FHA i
-
-select * from THESISi.dbo.FHA
-
-ALTER TABLE THESISi.dbo.FHA
-DROP column F2, F3, F5
+UPDATE THESISi.dbo.BC
+SET TotalYears = DATEDIFF(YEAR,since,'2022/01/07')
 
 
+FHA i
 
-delete from THESISi.dbo.FHA where FacilityName IS NULL
+SELECT * FROM THESISi.dbo.FHA
 
 ALTER TABLE THESISi.dbo.FHA
-add StartDateConverted Date;
+DROP COLUMN F2, F3, F5
 
---Update THESISi.dbo.FHA
---SET StartDateConverted = CONVERT(Date,StartDate)
-
---ALTER TABLE THESISi.dbo.FHA
---DROP column StartDate
-
-
+DELETE FROM THESISi.dbo.FHA WHERE FacilityName IS NULL
 
 ALTER TABLE THESISi.dbo.FHA
-add TotalDays int
+ADD StartDateConverted DATE;
 
---update THESISi.dbo.FHA
---SET TotalDays = DATEDIFF(day,StartDate,'2022/01/07')
+UPDATE THESISi.dbo.FHA
+SET StartDateConverted = CONVERT(DATE,StartDate)
 
+ALTER TABLE THESISi.dbo.FHA
+DROP column StartDate
 
---select StartDate,
---datediff(day,StartDate,'2022/01/07') AS TotalDays from THESISi.dbo.FHA
+ALTER TABLE THESISi.dbo.FHA
+ADD TotalDays INT
 
+UPDATE THESISi.dbo.FHA
+SET TotalDays = DATEDIFF(DAY,StartDate,'2022/01/07')
 
-
+SELECT StartDate,
+DATEDIFF(DAY,StartDate,'2022/01/07') AS TotalDays FROM THESISi.dbo.FHA
 
 ALTER TABLE THESISi.dbo.FHA
 add TotalYears int
 
---update THESISi.dbo.FHA
---SET TotalYears = DATEDIFF(year,StartDate,'2022/01/07')
+update THESISi.dbo.FHA
+SET TotalYears = DATEDIFF(YEAR,StartDate,'2022/01/07')
 
 
+FNHA 1
+
+SELECT * FROM THESISi.dbo.FNHA
 
 
+IHA 1
 
-
---FNHA 1
-
-select * from THESISi.dbo.FNHA
-
-
-
-
-
---IHA 1
-
-select * from THESISi.dbo.IHA
-
---ALTER TABLE THESISi.dbo.IHA
---DROP column [Days since start date]
-
+SELECT * FROM THESISi.dbo.IHA
 
 ALTER TABLE THESISi.dbo.IHA
-add TotalYears int
+DROP COLUMN [Days since start date]
 
-update THESISi.dbo.IHA
-SET TotalYears = DATEDIFF(year,[Advisory Start Date],'2022/01/07')
+ALTER TABLE THESISi.dbo.IHA
+ADD TotalYears INT
+
+UPDATE THESISi.dbo.IHA
+SET TotalYears = DATEDIFF(YEAR,[Advisory Start Date],'2022/01/07')
 WHERE TRY_CONVERT(DATE, [Advisory Start Date]) IS NOT NULL;
 
 
---MB 1
+MB 1
 
-select * from THESISi.dbo.MB
-
-ALTER TABLE THESISi.dbo.MB
-add SinceConverted Date;
-
---Update THESISi.dbo.MB
---SET SinceConverted = CONVERT(Date,Since)
-
---ALTER TABLE THESISi.dbo.MB
---DROP column Since
-
-
+SELECT * FROM THESISi.dbo.MB
 
 ALTER TABLE THESISi.dbo.MB
-add TotalDays int
+ADD SinceConverted DATE;
 
---update THESISi.dbo.MB
---SET TotalDays = DATEDIFF(day,since,'2022/01/07')
-
-
+UPDATE THESISi.dbo.MB
+SET SinceConverted = CONVERT(DATE,Since)
 
 ALTER TABLE THESISi.dbo.MB
-add TotalYears int
+DROP COLUMN Since
 
---update THESISi.dbo.MB
---SET TotalYears = DATEDIFF(year,since,'2022/01/07')
+ALTER TABLE THESISi.dbo.MB
+ADD TotalDays INT
+
+UPDATE THESISi.dbo.MB
+SET TotalDays = DATEDIFF(DAY,since,'2022/01/07')
+
+ALTER TABLE THESISi.dbo.MB
+ADD TotalYears INT
+
+UPDATE THESISi.dbo.MB
+SET TotalYears = DATEDIFF(YEAR,since,'2022/01/07')
 
 
---VCHA i
+VCHA i
 
-select * from dbo.vcha
-
+SELECT * FROM dbo.vcha
 
 ALTER TABLE dbo.vcha
-add StartDateConverted Date;
+ADD StartDateConverted DATE;
 
---Update dbo.vcha
---SET StartDateConverted = CONVERT(Date,StartDate)
+UPDATE dbo.vcha
+SET StartDateConverted = CONVERT(DATE,StartDate)
 
---ALTER TABLE dbo.vcha
---DROP column StartDate
+ALTER TABLE dbo.vcha
+DROP COLUMN StartDate
 
 
+VIHA i
 
---VIHA i
-
-select * from THESISi.dbo.viha
+SELECT * FROM THESISi.dbo.viha
 
 ALTER TABLE THESISi.dbo.viha
-DROP column Today, F2, F5
+DROP COLUMN Today, F2, F5
 
 
+2022.09.28
 
 
+AB ii
+
+SELECT * FROM THESISii.dbo.AB
 
 
+FHA ii
 
---2022.09.28
-
-
---AB ii
-
-select * from THESISii.dbo.AB
-
-
-
---FHA ii
-
-select * from THESISii.dbo.FHA
+SELECT * FROM THESISii.dbo.FHA
 
 ALTER TABLE THESISii.dbo.FHA
 add StartDateConverted Date;
 
---Update THESISii.dbo.FHA
---SET StartDateConverted = CONVERT(Date,StartDate)
+UPDATE THESISii.dbo.FHA
+SET StartDateConverted = CONVERT(DATE,StartDate)
 
---ALTER TABLE THESISii.dbo.FHA
---DROP column StartDate
-
-
---FNHA ii
-
-select * from THESISii.dbo.FNHA
-
-ALTER TABLE THESISii.dbo.FNHA
-add DateSetConverted Date;
-
---Update THESISii.dbo.FNHA
---SET DateSetConverted = CONVERT(Date,DateSet)
-
---ALTER TABLE THESISii.dbo.FNHA
---DROP column DateSet
+ALTER TABLE THESISii.dbo.FHA
+DROP COLUMN StartDate
 
 
+FNHA ii
+
+SELECT * FROM THESISii.dbo.FNHA
 
 ALTER TABLE THESISii.dbo.FNHA
-add DateRevokedConverted Date;
+ADD DateSetConverted DATE;
 
---Update THESISii.dbo.FNHA
---SET DateRevokedConverted = CONVERT(Date,DateRevoked)
+UPDATE THESISii.dbo.FNHA
+SET DateSetConverted = CONVERT(DATE,DateSet)
 
---ALTER TABLE THESISii.dbo.FNHA
---DROP column DateRevoked
+ALTER TABLE THESISii.dbo.FNHA
+DROP COLUMN DateSet
 
+ALTER TABLE THESISii.dbo.FNHA
+ADD DateRevokedConverted DATE;
 
+UPDATE THESISii.dbo.FNHA
+SET DateRevokedConverted = CONVERT(DATE,DateRevoked)
 
---IHA 2
-
-select * from THESISii.dbo.IHA
-
---MB 2
-
-select * from THESISii.dbo.MB
-
---SK 2
-
-select * from THESISii.dbo.SK
+ALTER TABLE THESISii.dbo.FNHA
+DROP COLUMN DateRevoked
 
 
+IHA ii
 
---2023.06.26
+SELECT * FROM THESISii.dbo.IHA
+
+MB ii
+
+SELECT * FROM THESISii.dbo.MB
+
+SK ii
+
+SELECT * FROM THESISii.dbo.SK
 
 
---AB
+2023.06.26
 
+AB iii
 
---2022.01.07
-
---AB i
-
-select * from THESISiii.dbo.AB
-
-ALTER TABLE THESISiii.dbo.AB
-add SinceConverted Date;
-
---Update THESISiii.dbo.AB
---SET SinceConverted = CONVERT(Date,Since)
+SELECT * FROM THESISiii.dbo.AB
 
 ALTER TABLE THESISiii.dbo.AB
-DROP column Since
+ADD SinceConverted DATE;
 
-
-
-
-
-ALTER TABLE THESISiii.dbo.AB
-add TotalDays int
-
---update THESISiii.dbo.AB
---SET TotalDays = DATEDIFF(day,since,'2023/06/26')
-
-
+UPDATE THESISiii.dbo.AB
+SET SinceConverted = CONVERT(DATE,Since)
 
 ALTER TABLE THESISiii.dbo.AB
-add TotalYears int
+DROP COLUMN Since
 
---update THESISiii.dbo.AB
---SET TotalYears = DATEDIFF(year,since,'2023/06/26')
+ALTER TABLE THESISiii.dbo.AB
+ADD TotalDays INT
+
+UPDATE THESISiii.dbo.AB
+SET TotalDays = DATEDIFF(DAY,since,'2023/06/26')
+
+ALTER TABLE THESISiii.dbo.AB
+ADD TotalYears INT
+
+UPDATE THESISiii.dbo.AB
+SET TotalYears = DATEDIFF(YEAR,since,'2023/06/26')
 
 
+IHA 3
 
---IHA 3
-
-select * from THESISiii.dbo.IHA
+SELECT * FROM THESISii.dbo.SK THESISiii.dbo.IHA
 
 ALTER TABLE THESISiii.dbo.IHA
-add StartDateConverted Date;
+ADD StartDateConverted DATE;
 
---Update THESISiii.dbo.IHA
---SET StartDateConverted = CONVERT(Date,[Start Date])
-
-ALTER TABLE THESISiii.dbo.IHA
-DROP column [Start Date]
-
-
+UPDATE THESISiii.dbo.IHA
+SET StartDateConverted = CONVERT(DATE,[Start Date])
 
 ALTER TABLE THESISiii.dbo.IHA
-add TotalYears int
+DROP COLUMN [Start Date]
 
-update THESISiii.dbo.IHA
-SET TotalYears = DATEDIFF(year,StartDate,'2023/06/26')
+ALTER TABLE THESISiii.dbo.IHA
+ADD TotalYears INT
+
+UPDATE THESISiii.dbo.IHA
+SET TotalYears = DATEDIFF(YEAR,StartDate,'2023/06/26')
 WHERE TRY_CONVERT(DATE, StartDate) IS NOT NULL;
 
 
+MB 3
 
---MB 3
-
-select * from THESISiii.dbo.MB
-
-ALTER TABLE THESISiii.dbo.MB
-add SinceConverted Date;
-
---Update THESISiii.dbo.MB
---SET SinceConverted = CONVERT(Date,Since)
+SELECT * FROM THESISiii.dbo.MB
 
 ALTER TABLE THESISiii.dbo.MB
-DROP column Since
+ADD SinceConverted DATE;
 
-
-
-ALTER TABLE THESISiii.dbo.MB
-add TotalDays int
-
---update THESISiii.dbo.MB
---SET TotalDays = DATEDIFF(day,since,'2023/06/26')
-
-
+UPDATE THESISiii.dbo.MB
+SET SinceConverted = CONVERT(DATE,Since)
 
 ALTER TABLE THESISiii.dbo.MB
-add TotalYears int
+DROP COLUMN Since
 
---update THESISiii.dbo.MB
---SET TotalYears = DATEDIFF(year,since,'2023/06/26')
+ALTER TABLE THESISiii.dbo.MB
+ADD TotalDays INT
+
+UPDATE THESISiii.dbo.MB
+SET TotalDays = DATEDIFF(DAY,since,'2023/06/26')
+
+ALTER TABLE THESISiii.dbo.MB
+ADD TotalYears INT
+
+UPDATE THESISiii.dbo.MB
+SET TotalYears = DATEDIFF(YEAR,since,'2023/06/26')
