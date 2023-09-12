3af6a8018dd9129d186bdd14f3561ecd639e9822
reginald-todd <142221302+reginald-todd@users.noreply.github.com>
2023-09-12 16:30:58 +0200

    Add files via upload

---
+++ tableau total DWA per authority or province.sql
@@ -0,0 +1,74 @@
+SELECT COUNT(Type) AS 'AB 1 Total'
+FROM Thesisi.dbo.AB
+
+SELECT COUNT(Type) AS 'AB 3 Total'
+FROM Thesisiii.dbo.AB
+
+SELECT COUNT(Type) AS 'BC 1 Total'
+FROM Thesisi.dbo.BC
+
+SELECT COUNT(Type) AS 'BC 3 Total'
+FROM Thesisiii.dbo.BC
+
+SELECT COUNT(Location) AS 'FHA 1 Total'
+FROM Thesisi.dbo.FHA
+
+SELECT COUNT(Location) AS 'FHA 2 Total'
+FROM Thesisii.dbo.FHA
+
+SELECT COUNT(Location) AS 'FHA 3 Total'
+FROM Thesisiii.dbo.FHA
+
+SELECT COUNT(Duration) AS 'FNHA 1 Total'
+FROM Thesisi.dbo.FNHA
+
+SELECT COUNT(Duration) AS 'FNHA 2 Total'
+FROM Thesisii.dbo.FNHA
+
+SELECT COUNT(Duration) AS 'FNHA 3 Total'
+FROM Thesisiii.dbo.FNHA
+
+SELECT COUNT(Website) AS 'IHA 1 Total'
+FROM Thesisi.dbo.IHA
+
+SELECT COUNT(Website) AS 'IHA 2 Total'
+FROM Thesisii.dbo.IHA
+
+SELECT COUNT(Website) AS 'IHA 3 Total'
+FROM Thesisiii.dbo.IHA
+
+SELECT COUNT(Type) AS 'NHA 2 Total'
+FROM Thesisii.dbo.NHA
+
+SELECT COUNT(Type) AS 'NHA 3 Total'
+FROM Thesisiii.dbo.NHA
+
+SELECT COUNT(TotalYears) AS 'MB 1 Total'
+FROM Thesisi.dbo.MB
+
+SELECT COUNT(TotalYears) AS 'MB 3 Total'
+FROM Thesisiii.dbo.MB
+
+SELECT COUNT(Reason) AS 'SK 1 Total'
+FROM Thesisi.dbo.SK
+
+SELECT COUNT(Reason) AS 'SK 3 Total'
+FROM Thesisiii.dbo.SK
+
+SELECT COUNT('Reason(s)') AS 'VCHA 1 Total'
+FROM Thesisi.dbo.VCHA
+
+SELECT COUNT(Location) AS 'VCHA 2 Total'
+FROM Thesisii.dbo.VCHA
+
+SELECT COUNT('Reason(s)') AS 'VCHA 3 Total'
+FROM Thesisiii.dbo.VCHA
+
+SELECT COUNT(Location) AS 'VIHA 1 Total'
+FROM Thesisi.dbo.VIHA
+
+SELECT COUNT(Location) AS 'VIHA 2 Total'
+FROM Thesisii.dbo.VIHA
+
+SELECT COUNT(Location) AS 'VIHA 3 Total'
+FROM Thesisiii.dbo.VIHA
